7ff545d58bfc8e1f48ac27182d39b08e72132c10
Sergey Novikov <vizzeb@gmail.com>
2022-07-27 17:55:15 +0300

    Socket

--- src/test/java/ServerTest.java
+++ src/test/java/ServerTest.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 
 public class ServerTest {
-
+    private static int usersCount = 4;
     @Test
     public void testRun() throws IOException {
-        Server server = new Server();
+        Server server = new Server(usersCount);
         server.start();
         Assertions.assertTrue(server.isAlive());
     }
